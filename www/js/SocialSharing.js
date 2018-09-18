c9883bedfa13cad06b8a75474bc952484d12f442
surajsinghnegi <surajsinghnegi009@gmail.com>
2018-09-18 08:10:37 +0530

    1

---
+++ www/js/SocialSharing.js
@@ -0,0 +1,39 @@
+function fbShare(){
+  window.plugins.socialsharing.shareViaFacebook('Message via Facebook', 
+                                                 null /* img */, 
+                                                 null /* url */, 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+};
+
+function whatsappShare(){
+  window.plugins.socialsharing.shareViaWhatsApp('Message via WhatsApp',
+                                                 null /* img */,
+                                                 "https://pointdeveloper.com/" /* url */, 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+};
+
+function twitterShare(){
+   window.plugins.socialsharing.shareViaTwitter('Message via Twitter',
+                                                 null /* img */, 
+                                                 'http://twitter.com/', 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+  };
+
+ function otherShare(){
+   window.plugins.socialsharing.share('My message',
+      'PhoneGap share plugin',
+      'https://www.google.nl/images/srpr/logo4w.png',
+      'http://www.x-services.nl');
+  };
+
+ function storyShare(){ 
+   var data = $('.text-flow').text();
+   window.plugins.socialsharing.share('Download app for more hindi stories \n https://play.google.com/store/apps/details?id=com.devsun.zombienight \n\n'+data, null, null, null);
+  };
+
